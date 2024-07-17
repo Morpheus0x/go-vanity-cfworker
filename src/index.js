00b9bfa4bda4f5610980acf61c5654d7d10b0f86
Morpheus0x <morpheus0x@pm.me>
2024-07-17 18:47:23 +0200

    FIX redirect timer to 3 seconds

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ async function handleRequest(request) {
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="go-import" content="${importClause}" />
     <meta name="go-source" content="${sourceClause}" />
-    <meta http-equiv="refresh" content="0; url=${pkgURL}" />
+    <meta http-equiv="refresh" content="3; url=${pkgURL}" />
   </head>
   <body>
     Nothing to see here! <a href="${pkgURL}">Move along</a>
